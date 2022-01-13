24dc2498f2c30f6c22fb962e492b3c60b08376b5
Keldiyor1303 <keldiyorraxmatov1303@gmail.com>
2022-01-13 20:09:32 +0500

    section-1 add

---
+++ JS/test.js
@@ -0,0 +1,83 @@
+const testlar = [
+    {
+        savol: "opacity xususiyati qanday qiymatlarini olishi mumkin?",
+        variant1: "0-1",
+        variant2: "0-10",
+        variant3: "0-50",
+        variant4: "0-100",
+        javob: 1
+    },
+    {
+        savol: "Semantik teglar qatorini toping?",
+        variant1: "<main>, <div>, <section>",
+        variant2: "<footer>, <article>, <ul>",
+        variant3: "<section>, <nav>, <aside>",
+        variant4: "<div>, <header>, <nav>",
+        javob: 3
+    },
+    {
+        savol: "<h1> tegining uzunligi qancha bo`lishi kerak?",
+        variant1: "10 tadan 60 tagacha",
+        variant2: "20 tadan 70 tagacha",
+        variant3: "15 tadan 75 tagacha",
+        variant4: "Istalgan uzunlikda",
+        javob: 2
+    },
+    {
+        savol: "<a> tegining qaysi attributi faylni yuklab oladi?",
+        variant1: "save",
+        variant2: "add",
+        variant3: "download",
+        variant4: "target",
+        javob: 3
+    },
+    {
+        savol: "console.log(false + true)",
+        variant1: "0",
+        variant2: "1",
+        variant3: "true",
+        variant4: "false",
+        javob: 2
+    },
+    {
+        savol: "console.console.log(Math.pow(4, 2))",
+        variant1: "6",
+        variant2: "16",
+        variant3: "32",
+        variant4: "64",
+        javob: 2
+    },
+    {
+        savol: "Quyidagilardan qaysi biri inline element?",
+        variant1: "span",
+        variant2: "div",
+        variant3: "hr",
+        variant4: "form",
+        javob: 1
+    },
+    {
+        savol: "console.log(2 ** 3 ** 2)",
+        variant1: "64",
+        variant2: "256",
+        variant3: "512",
+        variant4: "1024",
+        javob: 3
+    },
+    {
+        savol: "Transformning qaysi parametri elementni kattalashtiradi?",
+        variant1: "translate()",
+        variant2: "rotate()",
+        variant3: "scale()",
+        variant4: "skew()",
+        javob: 3
+    },
+    {
+        savol: "Qaysi metod yordamida massiv boshiga element qo`shish mumkin",
+        variant1: "unshift()",
+        variant2: "shift()",
+        variant3: "pop()",
+        variant4: "push()",
+        javob: 1
+    },
+]
+
